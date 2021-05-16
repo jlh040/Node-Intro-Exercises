304ffe0e656a873cef66b38b023c39c6414c02ed
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-16 14:27:47 -0700

    added functions so that data can now be output to a file

--- step3.js
+++ step3.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const axios = require('axios');
 
-const cat = path => {
+function cat(path) {
     fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
             console.log(`Error reading ${path}:`, err);
@@ -22,19 +22,62 @@ async function webcat(url) {
     }
 }
 
-const nodeCat = () => {
-    let arg = process.argv[2];
-    if (isURL(arg)) {
-        webcat(arg);
-    } else{
-        cat(arg);
+async function nodeCat() {
+    let args = process.argv;
+    if (args[2] === '--out') {
+        write(args[3], args[4]);
+    }
+    else if (isURL(args[2])) {
+        webcat(args[2]);
+    }
+    else {
+        cat(args[2]);
     }
 }
 
-function isURL(arg) {
+const isURL = arg => {
     if (arg.includes('http://') || arg.includes('.com')) {
         return true;
     }
+}
+
+async function write(writeTo, readFrom) {
+    if (isURL(readFrom)) {
+        fs.writeFile(writeTo, await getTextFromURL(readFrom), 'utf8', err => {
+            if (err) {
+                console.log(`Couldn't write ${writeTo}: ${err}`);
+                process.exit(1)
+            }
+        })
+    } 
+    else {
+    fs.writeFile(writeTo, await getTextFromFile(readFrom), 'utf8', err => {
+        if (err) {
+            console.log(`Couldn't write ${writeTo}: ${err}`);
+            process.exit(1);
+        }
+    })}
+}
+
+function getTextFromFile(path) {
+    return new Promise((resolve, reject) => fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log(`Could not read from ${path}: ${err}`)
+            process.exit(1)
+        }
+        resolve(data);
+    }))
+}
+
+function getTextFromURL(url) {
+    return new Promise(async function(resolve, reject) {
+        try {
+            resp = await axios.get(url)
+            resolve(resp['data']);
+        } catch(e) {
+            console.log(`Error fetching ${url}: ${e}`)
+        }
+    })
 }
 
 nodeCat()
