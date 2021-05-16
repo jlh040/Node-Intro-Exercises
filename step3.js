92ee414072fd139ab203abf7e94b1b0819bb667e
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-16 11:17:53 -0700

    copied the code from step2.js into step3.js

---
+++ step3.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const axios = require('axios');
+
+const cat = path => {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log(`Error reading ${path}:`, err);
+            process.exit(1);
+        }
+        else {
+            console.log(data);
+        }
+    })
+}
+
+async function webcat(url) {
+    try {
+        const resp = await axios.get(url);
+        console.log(resp['data']);
+    } catch(e) {
+        console.log(`Error fetching ${url}: ${e}`)
+    }
+}
+
+const nodeCat = () => {
+    let arg = process.argv[2];
+    if (isURL(arg)) {
+        webcat(arg);
+    } else{
+        cat(arg);
+    }
+}
+
+function isURL(arg) {
+    if (arg.includes('http://') || arg.includes('.com')) {
+        return true;
+    }
+}
+
+nodeCat()
